ec51375aeff44684442fec684079f94c73938fba
C. West <clairew4@mox1.hyak.local>
2021-02-03 11:22:40 -0800

    fix launch name

--- useful_scripts/make_spectrum_image/launch_full.sh
+++ useful_scripts/make_spectrum_image/launch_full.sh
@@ -5,8 +5,8 @@ zend=110
 ss=20
 
 for ((z=$zstart;z<=$zend;z+=$ss));do
-    cp launch_temp launch_temp_${z}
-    sed -i "s/name=test/name=sp_${z}/g" launch_temp_${z}
-    sed -i "s/z=0/z=${z}/g" launch_temp_${z}
-    sbatch launch_temp_${z}
+    cp launch.slurm launch_${z}.slurm
+    sed -i "s/name=test/name=sp_${z}/g" launch_${z}.slurm
+    sed -i "s/z=0/z=${z}/g" launch_${z}.slurm
+    sbatch launch_${z}.slurm
 done 
